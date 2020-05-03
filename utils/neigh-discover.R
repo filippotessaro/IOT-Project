f7e67ff7e5116c5f3b0e004584500bf428f85361
filippotessaro <filippotessaro96@gmail.com>
2020-05-03 16:37:10 +0200

    ended plotting

--- utils/neigh-discover.R
+++ utils/neigh-discover.R
@@ -60,6 +60,10 @@ df <- data.frame()
 
 nodes = c(2, 5, 10, 20, 30, 50)
 
+#nodes = c(2, 5)
+sims <- c(0,1,2,3,4)
+
+
 # computes the offered load
 neighbour.discovery.rate <- function(df, ID, lower.bound, upper.bound) {
   temp.df = subset(df, ( Epoch >= lower.bound & Epoch <= upper.bound))
@@ -67,25 +71,31 @@ neighbour.discovery.rate <- function(df, ID, lower.bound, upper.bound) {
   return(temp.df)
 }
 
-
-for(i in nodes){
-  print(paste("Preproc file:",i, sep=" "))
-  file.name = paste(i,"epoch.csv",sep="")
-  print(file.name)
-  temp.df = read.table(paste("../dst_test/burst_20task_100off_150send/", file.name, sep=""),
-                       header = TRUE,
-                       sep = ",")
-  temp.df$config_nodes = rep(i, nrow(temp.df))
-  
-  # if first time 
-  # bind cols names
-  if (length(df) == 0){
-    df = temp.df
-  } else{
-    df <- rbind(df, temp.df)
+for (j in sims){
+  for(i in nodes){
+    print(paste("Preproc sim:",j, sep=" "))
+    
+    print(paste("Preproc file:",i, sep=" "))
+    file.name = paste(i,"epoch.csv",sep="")
+    folder = paste("../dst_test/burst_20task_100off_250send_sim", j, sep="")
+    folder = paste(folder, "/", sep="")
+    print(file.name)
+    temp.df = read.table(paste(folder, file.name, sep=""),
+                         header = TRUE,
+                         sep = ",")
+    temp.df$config_nodes = rep(i, nrow(temp.df))
+    
+    # if first time 
+    # bind cols names
+    if (length(df) == 0){
+      df = temp.df
+    } else{
+      df <- rbind(df, temp.df)
+    }
+    
   }
-  
 }
+
 
 # remove first 10 epoch and over 100
 filtered.df <- neighbour.discovery.rate(df,1,10,100)
@@ -106,30 +116,13 @@ tibble(nodeslabel, by_epoch$avg) %>%
          , col = factor(sign(by_epoch$avg))) %>% 
   ggplot(aes(x =  reorder(nodeslabel, by_epoch$config_nodes) , y = by_epoch$percentage)) +
   geom_col(aes(fill = col)) +
-  geom_label(aes(y = lab_loc, label = round(by_epoch$percentage, 2))) +
-  geom_text(aes(y = lab_loc, label = round(by_epoch$avg, 2)), color = "white", size = 4, position = position_stack(vjust = 0.5)) +
+  geom_label(aes(label = round(by_epoch$percentage, 2)), position = position_stack(vjust = 1.0)) +
+  geom_text(aes(label = round(by_epoch$avg, 2)), color = "white", size = 4, position = position_stack(vjust = 0.5)) +
   scale_fill_manual(name = 'sign', values = c('1' = '#6FAA46')) +
   ylab('Percentage') +
   xlab('Nodes') +
   ggtitle('Neighbour Dicovery Rate') +
   theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # *************************************************
 # *
@@ -142,51 +135,75 @@ dc.df <- data.frame()
 #names(dc.df) <- col_headings
 #data.frame(Nodes=NA, AVG_ON=NA, AVG_TX=NA, AVG_RX=NA, AVG_INT=NA)[numeric(0), ]
 
-
-for(i in nodes){
-  print(paste("Preproc file:",i, sep=" "))
-  file.name = paste(i,"power.log",sep="")
-  print(file.name)
-  file.name = paste("../dst_test/scatter/scatter_30task/raw/", file.name, sep="")
-  
-  temp.df <- data.frame()
-  
-  con <- file(file.name, "r")
-  lines.list <- readLines(con,n=4)
-  list.vals <- list()
-  
-  for(line in lines.list){
-    tmp.list <- unlist(strsplit(line, " "))
-    #list.vals
-    print(tmp.list[5])
-    list.vals[length(list.vals) +1 ] <- tmp.list[5]
+for (j in sims){
+  for(i in nodes){
+    print(paste("Preproc file:",i, sep=" "))
+    #file.name = paste(i,"power.log",sep="")
+    #print(file.name)
+    
+    file.name = paste(i,"power.log",sep="")
+    folder = paste("../dst_test/burst_20task_100off_250send_sim", j, sep="")
+    folder = paste(folder, "/raw/", sep="")
+    print(file.name)
+    
+    file.name = paste(folder, file.name, sep="")
+    
+    temp.df <- data.frame()
+    
+    con <- file(file.name, "r")
+    lines.list <- readLines(con,n=4)
+    list.vals <- list()
+    
+    for(line in lines.list){
+      tmp.list <- unlist(strsplit(line, " "))
+      #list.vals
+      print(tmp.list[5])
+      list.vals[length(list.vals) +1 ] <- tmp.list[5]
+    }
+    close(con)
+    
+    temp.df <- data.frame(matrix(unlist(list.vals), nrow=1, byrow=F))
+    
+    temp.df$config_nodes = rep(i, nrow(temp.df))
+    
+    # if first time 
+    # bind cols names
+    dc.df <- rbind(dc.df, temp.df)
+    
   }
-  close(con)
-  
-  temp.df <- data.frame(matrix(unlist(list.vals), nrow=1, byrow=F))
-  
-  temp.df$config_nodes = rep(i, nrow(temp.df))
-  
-  # if first time 
-  # bind cols names
-  dc.df <- rbind(dc.df, temp.df)
-  
-  
-}
+}  
 
-library(dplyr)
-dc.df %>% 
-  mutate_all(funs(as.numeric(as.numeric(.))))
+dc.df <- dc.df %>% group_by(config_nodes) %>% summarise(AVG_ON = mean(as.numeric(as.character(X1))), 
+                                               AVG_TX = mean(as.numeric(as.character(X2))), 
+                                               AVG_RX = mean(as.numeric(as.character(X3))), 
+                                               AVG_INT = mean(as.numeric(as.character(X4))))
 
 
-ggplot(data = dc.df, aes(x = dc.df$config_nodes , y = dc.df$X1)) +
-geom_col(aes(fill = dc.df$config_nodes)) +
+
+tibble(nodeslabel, statistics$AVG_ON) %>% 
+  mutate(lab_loc = statistics$AVG_ON
+         , col = factor(sign(statistics$AVG_ON))) %>% 
+  ggplot(aes(x =  reorder(nodeslabel, statistics$config_nodes) , y = statistics$AVG_ON)) +
+  geom_col(aes(fill = col)) +
+  geom_label(aes(label = round(statistics$AVG_ON, 2)), position = position_stack(vjust = 1.0)) +
+  #geom_text(aes(label = round(by_epoch$avg, 2)), color = "white", size = 4, position = position_stack(vjust = 0.5)) +
+  #scale_fill_manual(name = 'sign', values = c('1' = '#6FAA46')) +
+  ylab('Percentage') +
+  xlab('Nodes') +
+  ggtitle('AVG ON') +
+  ylim(0, 100) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
+
+
+
+#ggplot(data = dc.df, aes(x = dc.df$config_nodes , y = dc.df$X1)) +
+#geom_col(aes(fill = dc.df$config_nodes)) +
 #geom_label(aes(y = lab_loc, label = round(dc.df$X1, 2))) +
 #geom_label(aes(y = lab_loc, label = round(by_epoch$percentage, 2))) +
 #geom_text(aes(y = lab_loc, label = round(by_epoch$avg, 2)), color = "white", size = 4, position = position_stack(vjust = 0.5)) +
 #scale_fill_manual(name = 'sign', values = c('1' = '#6FAA46')) +
-ylab('AVG ON') +
-xlab('Nodes') +
-ggtitle('AVG On') +
-theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
+#ylab('AVG ON') +
+#xlab('Nodes') +
+#ggtitle('AVG On') +
+#theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
   
